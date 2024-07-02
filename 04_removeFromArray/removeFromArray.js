0d017a3e62271ac136f3026c0cd7977e118184c3
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-02 13:56:37 +0530

    write remove from array function for the tests to work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    // for (const i of args){
+    //     if(array.includes(i)){
+    //         array=array.filter(x=>x!==i);
+    //     }
+    // }
+
+    return array.filter(arr=>!args.includes(arr));
 };
 
 // Do not edit below this line
