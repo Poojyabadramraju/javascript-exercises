56f301a987b2d08ae2e1559b5247eb7830151be2
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-02 13:56:03 +0530

    write reverse function for the tests to work

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArr=string.split("").reverse();
+    let str=strArr.join("");
+    return str;
 };
 
 // Do not edit below this line
