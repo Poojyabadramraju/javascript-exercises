3538895f845c13a94fce16b19669bd0b7a793641
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-02 13:58:10 +0530

    write sum all function for the tests to work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum=0;
+    if((num1<0 || num1!==parseInt(num1,10)) || (num2<0 || num2!==parseInt(num2,10))) return 'ERROR';
+    let i=((num1<num2)?num1:num2);
+    let j=((num1>num2)?num1:num2);
+    for(;i<=j;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
