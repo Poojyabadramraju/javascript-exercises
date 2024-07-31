3377b2afe0e57d809cd40cdf268221b7c099dab2
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-08-01 00:09:39 +0530

    write findTheOldest function for the tests to pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+    .map((person)=>{
+        if(!person.yearOfDeath){
+            person.yearOfDeath=new Date().getFullYear();
+        }
+        return person;
+    })
+    .toSorted((a,b)=>{
+        const prevPersonAge=a.yearOfDeath-a.yearOfBirth;
+        const nextPersonAge=b.yearOfDeath-b.yearOfBirth;
+        return (nextPersonAge-prevPersonAge);
+    })[0];
 };
 
 // Do not edit below this line
