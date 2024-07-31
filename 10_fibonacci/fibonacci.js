8f31e4e60feadc92a8760b1cdc484761310710ea
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-31 20:43:46 +0530

    write fibonacci function for the tests to work

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(numN) {
+    let num1=num2=1;
+    let sum=0;
+    numN=parseInt(numN);
+    if(numN===0){
+        return 0;
+    }else if(numN===1 || numN===2){
+        return 1;
+    }else if(numN<0){
+        return "OOPS";
+    }
+    for(i=3;i<=numN;i++){
+        sum=num1+num2;
+        [num1,num2]=[num2,sum]
+    }
+    return num2;
 };
 
 // Do not edit below this line
