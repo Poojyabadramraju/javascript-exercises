bb58c8e2db59871a53768b178cf8929cf5516f7e
Poojyabadramraju <poojyabadramraju@gmail.com>
2024-07-31 19:43:44 +0530

    write palindrome function for the tests to work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical="abcdefghijklmnopqrstuvwxyz0123456789";
+    let newString=string
+    .toLowerCase()
+    .split("")
+    .filter((char)=>alphanumerical.includes(char))
+    .join("");
+    let reverseString=newString.split("").reverse().join("");
+    return newString===reverseString;
 };
 
 // Do not edit below this line
